3bcf34d9803f5081eda3b6861ed2efaad22c9f85
Aidina Ormusheva <aidina.190897@gmail.com>
2020-02-09 11:11:03 +0600

    first commit

---
+++ app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.android.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new MondayFragment();
+        } else if (position == 1){
+            return new TuesdayFragment();
+        } else {
+            return new WednesdayFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
